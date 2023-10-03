7036ac725b08560a7d625d9478c15e2b3b3622d5
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2023-10-03 15:34:26 -0400

    Resolvendo problemas de formulário

--- web/src/components/Horario.jsx
+++ web/src/components/Horario.jsx
@@ -3,133 +3,92 @@ import { Button, Card, Form, Modal, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { Input } from "./Input";
 
+export function EditHorario(props) {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm(); // Corrigi "formsState" para "formState"
 
-export function Horario(props) {
-  const { handleSubmit, register, formState: { errors } } = useForm(); // Corrigi "formsState" para "formState"
-  const [isUpdated, setIsUpdated] = useState(false);
-
-  async function editHorario (data) {
-    await props.editHorario ({ ...data, id: props.horario.id });
-    setIsUpdated (false);
-  }
-
-  /*
-  const [horarios, setHorarios] = useState([]);
-  const [selectedHorario, setSelectedHorario] = useState(null);
-
-  
-  const refreshHorarios = async () => {
-    const response = await getHorarios();
-    if (response.status === 200) {
-      setHorarios(response.data);
+    async function editHorario(data) {
+        await props.editHorario({ ...data, id: props.horario.id });
+        props.setIsUpdated(false);
     }
-  };
 
-  useEffect(() => {
-    refreshHorarios();
-  }, []);
+    return (
+        <Modal show={props.isUpdated} onHide={() => props.setIsUpdated(false)}>
+            <Card className="mb-3 p-3 bg-light">
+                <Card.Text>
+                    <strong>Horário de Partida: </strong>
+                    {props.horario.Horario_Partida}
+                </Card.Text>
+                <Card.Text>
+                    <strong>Horário de Chegada: </strong>
+                    {props.horario.Horario_Chegada}
+                </Card.Text>
+            </Card>
+            <Modal.Header>
+                <Modal.Title>
+                    Editar Horário: Partida - {props.horario.Horario_Partida},
+                    Chegada - {props.horario.Horario_Chegada}
+                </Modal.Title>
+            </Modal.Header>
 
-  const handleCreateHorario = async (data) => {
-    const response = await createHorario(data);
-    if (response.status === 201) {
-      refreshHorarios();
-      setIsModalOpen(false);
-    }
-  };
-
-  const handleEditHorario = async (data) => {
-    if (!selectedHorario) return;
-    data.id = selectedHorario.id;
-    const response = await updateHorario(data);
-    if (response.status === 200) {
-      refreshHorarios();
-      setIsModalOpen(false);
-    }
-  };
-
-  const handleDeleteHorario = async () => {
-    if (!selectedHorario) return;
-
-    const response = await deleteHorario(selectedHorario.id);
-    if (response.status === 200) {
-      refreshHorarios();
-      setIsModalOpen(false);
-    }
-  };
-  */
-
-  return (
-    <>
-      <h1>Horários</h1>
-      <Card className="mb-3 p-3 bg-light">
-      
-        
-          <Card.Text><strong>Horário de Partida: </strong>{props.horario.Horario_Partida}</Card.Text>
-          <Card.Text><strong>Horário de Chegada: </strong>{props.horario.Horario_Chegada}</Card.Text>
-
-          <Row xs="auto" className="d-flex justify-content-end">
-
-            <Button variant="secondary" onClick={() => setIsUpdated(true)}>Editar</Button>
-            <Button variant="outline-danger" className="ms-3" onClick={props.removeHorario}>Apagar</Button> 
-          </Row>
-        </Card>
-      
-      <Modal show={isUpdated} onHide={() => { setIsUpdated(false) }}>
-        <Modal.Header>
-        <Modal.Title>Editar Horário: Partida - {props.horario.Horario_Partida}, Chegada - {props.horario.Horario_Chegada}</Modal.Title>
-
-        </Modal.Header>
-       
-
-        <Form noValidate onSubmit={handleSubmit(editHorario)} validated={!!errors}>
-          <Modal.Body>
-          
-          
-          
-
-            <Input
-              className="mb-3"
-              type='number'
-              defaultValue={props.horario.Horario_Partida}
-              label='Horario de partida'
-              placeholder='Insira o horário de partida '
-              required={true}
-              name='Horario_Partida'
-              error={errors.Horario_Partida}
-              validations={register('Horario_Partida', {
-                required: {
-                  value: true,
-                  message: 'Horário de partida é obrigatório.'
-                }
-              })}
-            />
-            <Input
-              className="mb-3"
-              type='number'
-              defaultValue={props.horario.Horario_Chegada}
-              label='Horario de chegada'
-              placeholder='Insira o horário de chegada '
-              required={true}
-              name='Horario_Chegada'
-              error={errors.Horario_Chegada}
-              validations={register('Horario_Chegada', {
-                required: {
-                  value: true,
-                  message: 'Horário de chegada é obrigatório.'
-                }
-              })}
-            />
-
-
-
-          </Modal.Body>
-          <Modal.Footer>
-          <Button variant="primary" type="submit">Editar</Button>
-          <Button variant="secondary" onClick={() => setIsUpdated(false)}>Fechar</Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-    </>
-  );
+            <Form
+                noValidate
+                onSubmit={handleSubmit(editHorario)}
+                validated={!!errors}
+            >
+                <Modal.Body>
+                    <Input
+                        className="mb-3"
+                        type="number"
+                        defaultValue={props.horario.Horario_Partida}
+                        label="Horario de partida"
+                        placeholder="Insira o horário de partida "
+                        required={true}
+                        name="Horario_Partida"
+                        error={errors.Horario_Partida}
+                        validations={register("Horario_Partida", {
+                            required: {
+                                value: true,
+                                message: "Horário de partida é obrigatório.",
+                            },
+                        })}
+                    />
+                    <Input
+                        className="mb-3"
+                        type="number"
+                        defaultValue={props.horario.Horario_Chegada}
+                        label="Horario de chegada"
+                        placeholder="Insira o horário de chegada "
+                        required={true}
+                        name="Horario_Chegada"
+                        error={errors.Horario_Chegada}
+                        validations={register("Horario_Chegada", {
+                            required: {
+                                value: true,
+                                message: "Horário de chegada é obrigatório.",
+                            },
+                        })}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        onClick={editHorario}
+                    >
+                        Editar
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={() => props.setIsUpdated(false)}
+                    >
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    );
 }
-
--- web/src/pages/Horario.jsx
+++ web/src/pages/Horario.jsx
@@ -1,208 +1,257 @@
-
 import React, { useEffect, useState } from "react";
-import { Container, Button, Modal, Form, Row, Col, Table } from "react-bootstrap";
+import {
+    Container,
+    Button,
+    Modal,
+    Form,
+    Row,
+    Col,
+    Table,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { Horario } from "../components/Horario";
+import { EditHorario } from "../components/Horario";
 import { Header } from "../components/Header";
-import { Input } from '../components/Input';
-import { createHorario, deleteHorario, getHorarios, updateHorario } from "../services/horario-service";
+import { Input } from "../components/Input";
+
+import {
+    createHorario,
+    deleteHorario,
+    getHorarios,
+    updateHorario,
+} from "../services/horario-service";
 
 export function Horarios() {
-  const [horarios, setHorarios] = useState([]);
-  const [isCreated, setIsCreated] = useState(false);
-  const { handleSubmit, register, formState: { errors } } = useForm();
-  const navigate = useNavigate();
+    const [horarios, setHorarios] = useState([]);
+    const [horarioSelecionado, setHorarioSelecionado] = useState({});
+    const [isCreated, setIsCreated] = useState(false);
+    const [isUpdated, setIsUpdated] = useState(false);
 
-  useEffect(() => {
-    findHorarios();
-    // eslint-disable-next-line
-  }, []);
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
 
+    const navigate = useNavigate();
 
-  async function filtrarHorario(horarioString) {
-    if (horarioString.length > 0) {
-      const horarioInteiro = parseInt(horarioString, 10);
+    useEffect(() => {
+        findHorarios();
+        // eslint-disable-next-line
+    }, []);
 
-      console.log(horarioInteiro);
+    async function filtrarHorario(horarioString) {
+        if (horarioString.length > 0) {
+            const horarioInteiro = parseInt(horarioString, 10);
 
-      if (!isNaN(horarioInteiro)) {
-        const resultadosFiltrados = horarios.filter(objeto => {
-          const horarioPartidaString = objeto.Horario_Partida.toString();
-          return horarioPartidaString.includes(horarioInteiro.toString());
-        });
+            console.log(horarioInteiro);
 
-        setHorarios(resultadosFiltrados);
-      } else {
-        console.log('A string não pôde ser convertida em um número inteiro.');
-      }
-    } else {
-      findHorarios();
+            if (!isNaN(horarioInteiro)) {
+                const resultadosFiltrados = horarios.filter((objeto) => {
+                    const horarioPartidaString =
+                        objeto.Horario_Partida.toString();
+                    return horarioPartidaString.includes(
+                        horarioInteiro.toString()
+                    );
+                });
+
+                setHorarios(resultadosFiltrados);
+            } else {
+                console.log(
+                    "A string não pôde ser convertida em um número inteiro."
+                );
+            }
+        } else {
+            findHorarios();
+        }
     }
-  }
 
-
-  async function findHorarios() {
-    try {
-      const result = await getHorarios();
-      setHorarios(result.data);
-    } catch (error) {
-      console.error(error);
-      navigate('/');
+    async function findHorarios() {
+        try {
+            const result = await getHorarios();
+            setHorarios(result.data);
+        } catch (error) {
+            console.error(error);
+            navigate("/");
+        }
     }
-  }
 
-  async function removeHorario(id) {
-    try {
-      await deleteHorario(id);
-      alert("Cadastro deletado com sucesso!");
-      await findHorarios();
-      debugger
-    } catch (error) {
-      console.error(error);
+    async function removeHorario(id) {
+        const answer = window.confirm(
+            "Tem certeza que deseja excluir este horário?"
+        );
+
+        if (answer) {
+            try {
+                await deleteHorario(id);
+                alert("Cadastro deletado com sucesso!");
+                await findHorarios();
+                debugger;
+            } catch (error) {
+                console.error(error);
+            }
+        }
     }
-  }
 
-  async function addHorario(data) {
-    try {
-
-      await createHorario(data);
-      setIsCreated(false);
-      alert("Cadastro feito com sucesso!");
-      await findHorarios();
-    } catch (error) {
-      console.error(error);
+    async function addHorario(data) {
+        try {
+            await createHorario(data);
+            setIsCreated(false);
+            alert("Cadastro feito com sucesso!");
+            await findHorarios();
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  async function editHorario(data) {
-
-    try {
-      await updateHorario({
-        id: data.id,
-        Horario_Partida: data.Horario_Partida,
-        Horario_Chegada: data.Horario_Chegada
-        // Adicione mais campos de edição conforme necessário
-      });
-      await findHorarios();
-      alert("Cadastro editado com sucesso!");
-    } catch (error) {
-      console.error(error);
+    async function editHorario(data) {
+        try {
+            await updateHorario({
+                id: data.id,
+                Horario_Partida: data.Horario_Partida,
+                Horario_Chegada: data.Horario_Chegada,
+                // Adicione mais campos de edição conforme necessário
+            });
+            await findHorarios();
+            alert("Cadastro editado com sucesso!");
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
+    return (
+        <Container fluid>
+            <Header title="Horários" />
+            <Row className="w-50 m-auto mb-5 mt-5">
+                <Col md="8">
+                    <Form.Control
+                        type="integer"
+                        onChange={(e) => filtrarHorario(e.target.value)}
+                        placeholder="Filtrar pelo Horario"
+                    />
+                </Col>
+                <Col md="4">
+                    <Button onClick={() => setIsCreated(true)}>
+                        Adicionar Novo Horário
+                    </Button>
+                </Col>
+            </Row>
 
+            <Col className="w-75 m-auto">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Horário de Partida</th>
+                            <th>Horário de Chegada</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {horarios && horarios.length > 0 ? (
+                            horarios.map((horario, index) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{horario.Horario_Partida}</td>
+                                    <td>{horario.Horario_Chegada}</td>
+                                    <td>
+                                        <Button
+                                            variant="primary"
+                                            onClick={() => {
+                                                setHorarioSelecionado(horario);
+                                                setIsUpdated(true);
+                                            }}
+                                        >
+                                            Editar
+                                        </Button>{" "}
+                                        <Button
+                                            variant="danger"
+                                            onClick={() =>
+                                                removeHorario(horario.id)
+                                            }
+                                        >
+                                            Excluir
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="4" className="text-center">
+                                    Não existe nenhum horário cadastrado!
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </Col>
 
+            <Modal show={isCreated} onHide={() => setIsCreated(false)}>
+                <Modal.Header>
+                    <Modal.Title>Adicionar Novo Horário</Modal.Title>
+                </Modal.Header>
 
-  return (
-    <Container fluid>
-      <Header title="Horários" />
-      <Row className="w-50 m-auto mb-5 mt-5">
-        <Col md="8">
-          <Form.Control
-            type="integer"
-            onChange={(e) => { filtrarHorario(e.target.value) }}
-            placeholder="Filtrar pelo Horario"
-          />
-        </Col>
-        <Col md="4">
-          <Button onClick={() => setIsCreated(true)}>Adicionar Novo Horário</Button>
-        </Col>
-      </Row>
+                <Form
+                    noValidate
+                    onSubmit={handleSubmit(addHorario)}
+                    validated={!!errors}
+                >
+                    <Modal.Body>
+                        <Form.Group controlId="Horario_Partida">
+                            <Form.Label>Horário de Partida</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Insira o horário de partida"
+                                required
+                                name="Horario_Partida"
+                                {...register("Horario_Partida", {
+                                    required:
+                                        "Horário de partida é obrigatório.",
+                                })}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.Horario_Partida?.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
+                        <Form.Group controlId="Horario_Chegada">
+                            <Form.Label>Horário de Chegada</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Insira o horário de chegada"
+                                required
+                                name="Horario_Chegada"
+                                {...register("Horario_Chegada", {
+                                    required:
+                                        "Horário de chegada é obrigatório.",
+                                })}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.Horario_Chegada?.message}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                    </Modal.Body>
 
-      <Col className="w-75 m-auto">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-             
-              <th>#</th>
-              <th>Horário de Partida</th>
-              <th>Horário de Chegada</th>
-              <th>Ações</th>
-         
-            </tr>
-          </thead>
-          <tbody>
-            {horarios && horarios.length > 0 ? (
-              horarios.map((horario, index) => (
-                <tr
-                  key={index}
-                 >
-                  <td>{index + 1}</td>
-                  <td>{horario.Horario_Partida}</td>
-                  <td>{horario.Horario_Chegada}</td>
-                  <td>
-                    <td>  </td>
+                    <Modal.Footer>
+                        <Button variant="primary" type="submit">
+                            Adicionar
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            onClick={() => setIsCreated(false)}
+                        >
+                            Fechar
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
 
-                  <td>    <Button variant="primary" onClick={() => editHorario(horario)}>Editar</Button>{' '}  </td>
-                  <td>  <Button variant="danger" onClick={() => removeHorario(horario.id)}>Excluir</Button>  </td>
-                  
-                   
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="4" className="text-center">Não existe nenhum horário cadastrado!</td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
-      </Col>
-      
-
-      
-
-
-      {/* Formulário dentro do Modal para adicionar novo horário */}
-      <Modal show={isCreated} onHide={() => setIsCreated(false)}>
-        <Modal.Header>
-          <Modal.Title>Adicionar Novo Horário</Modal.Title>
-        </Modal.Header>
-
-        <Form noValidate onSubmit={handleSubmit(addHorario)} validated={!!errors}>
-          <Modal.Body>
-            <Form.Group controlId="Horario_Partida">
-              <Form.Label>Horário de Partida</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Insira o horário de partida"
-                required
-                name="Horario_Partida"
-                {...register('Horario_Partida', {
-                  required: 'Horário de partida é obrigatório.'
-                })}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.Horario_Partida?.message}
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group controlId="Horario_Chegada">
-              <Form.Label>Horário de Chegada</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Insira o horário de chegada"
-                required
-                name="Horario_Chegada"
-                {...register('Horario_Chegada', {
-                  required: 'Horário de chegada é obrigatório.'
-                })}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.Horario_Chegada?.message}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="primary" type="submit">Adicionar</Button>
-            <Button variant="secondary" onClick={() => setIsCreated(false)}>Fechar</Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-    </Container>
-  );
-
+            <EditHorario
+                editHorario={editHorario}
+                setIsUpdated={setIsUpdated}
+                isUpdated={isUpdated}
+                horario={horarioSelecionado}
+            />
+        </Container>
+    );
 }
